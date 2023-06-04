c1776c28d6a154470a3921d48be72c6acf6c09df
Cristiano Santos <cristianossd@gmail.com>
2023-06-04 13:18:52 -0300

    fix: promo code generator

--- src/main/java/org/discount/coupons/Main.java
+++ src/main/java/org/discount/coupons/Main.java
@@ -6,19 +6,16 @@ public class Main {
     public static void main(String[] args) {
         try {
             CouponStore couponStore = new CouponStore();
-            couponStore.addCoupon(new Coupon("PROMO10", 10, 3));
-            couponStore.addCoupon(new Coupon("PROMO15", 15, 2));
-            couponStore.addCoupon(new Coupon("PROMO20", 20, 1));
+            String promoCode = PromoCodeGenerator.generate();
+            couponStore.addCoupon(new Coupon(promoCode, 20, 1));
 
             float firstPurchasePrice = (float) 1280.20;
-            String firstGivenCoupon = "PROMO20";
-            Coupon firstCoupon = couponStore.getCoupon(firstGivenCoupon);
+            Coupon firstCoupon = couponStore.getCoupon(promoCode);
             float firstFinalPrice = Checkout.applyDiscount(firstPurchasePrice, firstCoupon);
             System.out.println("First: final price is " + firstFinalPrice);
 
             float secondPurchasePrice = (float) 521.11;
-            String secondGivenCoupon = "PROMO20";
-            Coupon secondUsedCoupon = couponStore.getCoupon(firstGivenCoupon);
+            Coupon secondUsedCoupon = couponStore.getCoupon(promoCode);
             float secondFinalPrice = Checkout.applyDiscount(secondPurchasePrice, secondUsedCoupon);
         } catch (Exception e) {
             System.out.println("Unexpected behavior");
